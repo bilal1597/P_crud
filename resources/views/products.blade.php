5483389f90accc6d2b5c3dad80b146722c9e47c8
Bilal Ilyas <bilalilyas2597@gmail.com>
2024-12-02 19:22:03 +0500

    product table

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -20,10 +20,48 @@
 
     <body>
 
-        <h1>Products</h1>
+        <div class="container">
 
-        <div>
-            <a href="{{route('logout')}}"  class="btn btn-danger float-end " type="submit">Logout</a>
+                <div class="card card_header" >
+                    <br>
+                    <h1>All Products</h1>
+                    <div>
+                        <a href="{{route('logout')}}"  class="btn btn-danger float-end " type="submit">Logout</a><br><br>
+                    </div>
+                    <br>
+                <div class="card_body" >
+                    <table class="table table-hover ">
+                        <thead>
+                          <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                          </tr>
+                          <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                          </tr>
+                          <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>abc</td>
+                            <td>@twitter</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                </div>
+            </div>
         </div>
 
         <script
